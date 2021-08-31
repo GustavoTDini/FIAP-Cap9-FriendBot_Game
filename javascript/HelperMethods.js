a8f357e3044dffc75f77c08d7b20d9b100745815
GustavoTDini <gustavotrevisandini@gmail.com>
2021-08-31 18:19:02 -0300

    Studying Transform Road

--- javascript/HelperMethods.js
+++ javascript/HelperMethods.js
@@ -19,40 +19,44 @@ function invertAngle(currentAngle) {
 }
 
 function rideRoad(){
-    // ctx.setTransform(1,0,-3,1,0,0)
-    // ctx.drawImage(road, 0, 0,32,128,110,100, CUT_CANVAS,16)
-    // ctx.setTransform(1,0,-0.8,1,0,0)
-    // ctx.drawImage(road, 32, 0,32,128,105,100, CUT_CANVAS,16)
-    // ctx.setTransform(1,0,-0.6,1,0,0)
-    // ctx.drawImage(road, 64,0,32,128,105,100, CUT_CANVAS,16)
-    // ctx.setTransform(1,0,-0.4,1,0,0)
-    // ctx.drawImage(road,96,0,32,128,105,100, CUT_CANVAS,16)
-    // ctx.setTransform(1,0,-0.2,1,0,0)
-    // ctx.drawImage(road, 128, 0,32,128,105,100, CUT_CANVAS,16)
-    // ctx.setTransform(1,0,-1,1,0,0)
-    // ctx.drawImage(road, 0, 0,32,128,105,100, CUT_CANVAS,16)
-
-    // ctx.setTransform(0.9,0,(1*(3/32)-3),1,0,0)
-    // ctx.drawImage(road, CUT_SIZE*1+10, 0,CUT_SIZE,128,10*1+CUT_CANVAS*(1-1),100 + (1+1)*(1+1),(1+1)*(1+2),(1+1)*(1+2))
-    // ctx.setTransform(0.9,0,(2*(3/32)-3),1,0,0)
-    // ctx.drawImage(road, CUT_SIZE*2+10, 0,CUT_SIZE,128,10*1+CUT_CANVAS*(2-1),100 + (1+1)*(1+1),(1+1)*(1+2),(1+1)*(1+2))
-    // ctx.setTransform(0.9,0,(3*(3/32)-3),1,0,0)
-    // ctx.drawImage(road, CUT_SIZE*3+10, 0,CUT_SIZE,128,10*1+CUT_CANVAS*(3-1),100 + (1+1)*(1+1),(1+1)*(1+2),(1+1)*(1+2))
-    // ctx.setTransform(0.9,0,(4*(3/32)-3),1,0,0)
-    // ctx.drawImage(road, CUT_SIZE*4+10, 0,CUT_SIZE,128,10*1+CUT_CANVAS*(4-1),100 + (1+1)*(1+1),(1+1)*(1+2),(1+1)*(1+2))
-    // ctx.setTransform(0.9,0,(5*(3/32)-3),1,0,0)
-    // ctx.drawImage(road, CUT_SIZE*5+10, 0,CUT_SIZE,128,10*1+CUT_CANVAS*(5-1),100 + (j+1)*(j+1),(j+1)*(j+2),(j+1)*(j+2))
-    // ctx.setTransform(0.9,0,(6*(3/32)-3),1,0,0)
-    // ctx.drawImage(road, CUT_SIZE*6+10, 0,CUT_SIZE,128,10*1+CUT_CANVAS*(6-1),100 + (j+1)*(j+1),(j+1)*(j+2),(j+1)*(j+2))
-    for (let j = 1; j <= 8; j++){
+    // ctx.setTransform(1,0,-3,1,300,0)
+    // ctx.drawImage(road, 0, 0,
+    //                     256,128,
+    //                     0,100,
+    //                     640,128)
+    // ctx.setTransform(1,0,3,1,-300,0)
+    // ctx.drawImage(road, 256, 0,
+    //                     256,128,
+    //                     640,100,
+    //                     640,128)
+    // for (let j = 1; j <= 16; j++){
         for (let i = 0; i < 32; i++){
             ctx.setTransform(1,0,(i*(3/32)-3),1,0,0)
-            ctx.drawImage(road, CUT_SIZE*i, 0,CUT_SIZE,128,CUT_CANVAS*i,100+j*i,CUT_CANVAS + 4*j,j*j*8)
-            // ctx.setTransform(1,0,i*(3/32),1,0,0)
-            // ctx.drawImage(road, 256 + CUT_SIZE*i, 0,CUT_SIZE,128,CANVAS_WIDTH/2 + CUT_CANVAS*i,100 + CUT_CANVAS*j,CUT_CANVAS,j*j*4)
+            ctx.drawImage(road, 8*(i+1), 0,
+                                8,128,
+                                20*(i+1),100,
+                                40,128)
+            ctx.setTransform(1,0,i*(3/32),1,0,0)
+            ctx.drawImage(road, 256 + 8*(i+1), 0,
+                                8,128,
+                                640 + 20*(i+1),100,
+                                40,128)
         }
-    }
+    // }
 }
+
+// for (let i = 0; i < 32; i++) {
+//     ctx.setTransform(1, 0, (i * (3 / 32) - 3), 1, 0, 0)
+//     ctx.drawImage(road, 8 * (i + 1), 0,
+//         8, 128,
+//         20 * (i + 1), 100,
+//         20, 128)
+//     ctx.setTransform(1, 0, i * (3 / 32), 1, 0, 0)
+//     ctx.drawImage(road, 256 + 8 * (i + 1), 0,
+//         8, 128,
+//         640 + 20 * (i + 1), 100,
+//         20, 128)
+// }
 
 // para rotacionar os sprites
 function rotateSprite(ctx, angle, sprite, x, y) {
